6cce631e16f9613098f1acca38b1a6341c4330bf
Matheus Baldissara <baldissaramatheus@gmail.com>
2021-07-18 16:10:27 -0300

    feat: Expões headers

--- backend/index.ts
+++ backend/index.ts
@@ -8,6 +8,14 @@ async function startServer() {
   try {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true}))
+    app.use((req, res, next) => {
+      res.header(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+      );
+      res.header('Access-Control-Expose-Headers', 'Content-Range');
+      next();
+    });
     app.use('/news', newsRoutes);
     app.listen(port, () => {
       console.log(`Servidor sendo executado em https://localhost:${port}`);
