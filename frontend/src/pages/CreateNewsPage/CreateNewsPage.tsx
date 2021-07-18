f9480e8a2941e7810df8eccad69a8727d1a43fc8
Matheus Baldissara <baldissaramatheus@gmail.com>
2021-07-18 00:12:04 -0300

    fix: Inclui token na chamada de criacao de noticia

--- frontend/src/pages/CreateNewsPage/CreateNewsPage.tsx
+++ frontend/src/pages/CreateNewsPage/CreateNewsPage.tsx
@@ -3,15 +3,28 @@ import Container from '../../components/Container/Container';
 import Input from '../../components/Input/Input';
 import TextArea from '../../components/Input/TextArea';
 import Button from '../../components/Button/Button';
+import { AuthState } from '../../reducers/auth.reducer';
 import { History } from 'history';
 import './CreateNewsPage.css';
 import { API_URL } from '../../config/constants';
 import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-async function handleSubmit(event: React.FormEvent, history: History, title: string, content: string) {
+function mapStateToProps(state: AuthState) {
+	return { token: state.token };
+}
+
+async function handleSubmit(
+	event: React.FormEvent,
+	history: History,
+	title: string,
+	content: string,
+	token: string|undefined|null,
+) {
 	event.preventDefault();
 	const fetchConfig: RequestInit = {
 		method: 'POST',
+		headers: { Authorization: `Bearer ${token}` },
 		body: JSON.stringify({ title, content }),
 	}
 	const data = await fetch(API_URL, fetchConfig)
@@ -23,7 +36,7 @@ async function handleSubmit(event: React.FormEvent, history: History, title: str
 	history.push('/news/list');
 }
 
-const CreateNewsPage: React.FunctionComponent = () => {
+const CreateNewsPage: React.FunctionComponent<Partial<AuthState>> = (props: Partial<AuthState>) => {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
 
@@ -31,7 +44,7 @@ const CreateNewsPage: React.FunctionComponent = () => {
 
 	return (
 		<Container>
-			<form className="form" onSubmit={event => handleSubmit(event, history, title, content)}>
+			<form className="form" onSubmit={event => handleSubmit(event, history, title, content, props.token)}>
 				<Input label="Título" type="text" value={title} onChange={event => setTitle(event.target.value)} />
 				<TextArea label="Conteúdo" value={content} onChange={event => setContent(event.target.value)} />
 				<Button text="Enviar" />
@@ -40,4 +53,4 @@ const CreateNewsPage: React.FunctionComponent = () => {
 	);
 }
  
-export default CreateNewsPage;
+export default connect(mapStateToProps)(CreateNewsPage);
